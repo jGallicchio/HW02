275b221252c9c34a54ed9931026c21cd496c6c97
Justin Gallicchio <justin.gallicchio@uconn.edu>
2018-10-01 21:58:47 -0400

    Correct File

--- FindSet.py
+++ FindSet.py
@@ -18,45 +18,42 @@ if __name__ == '__main__':
 def Dict_create(lst):
     lst_dict = {}
     for x in range(len(lst)):
-        lst_dict[lst[item]] = x
+        lst_dict[lst[x]] = x
     return lst_dict
 
 def third_card(card1, card2):
-    card3 = [0,0,0,0]
-    for x in range(4):
+    card3 = []
+    for x in range(len(card1)):
         if (card1[x] == card2[x]):
-            card3[x] = card1[x]
-        elif (card1[x] + card2[x] == 3):
-            card3[x] = 0
-        elif (card1[x] + card2[x] == 2):
-            card3[x] = 1
-        elif (card1[x] + card2[x] == 1):
-            card3[x] = 2
+            card3.insert(x, str(card1[x]))
+        elif (int(card1[x]) + int(card2[x]) == 3):
+            card3.insert(x, '0')
+        elif (int(card1[x]) + int(card2[x]) == 2):
+            card3.insert(x, '1')
+        elif (int(card1[x]) + int(card2[x]) == 1):
+            card3.insert(x, '2')
     return ''.join(card3)
 
+#Time Complexity of Function = O(n^2)
 def findset(list):
-    cards = Dict_create(numbers)
-    third = ''
-    num_of_cards = 0
-    indA = 0
-    indB = 0
-    while indA < len(numbers):
-        while indB < len(numbers):
-            if (numbers[indA] != numbers[indB]):
-                third = third_card(numbers[indA], numbers[indB])
-                if (third in cards):
-                    num_of_cards = cards[third]
-                    print(cards[third])
-                    indB = len(numbers)
-                else:
-                    indB += 1
-            else:
-                indB += 1
-        if(num_of_cards == 0):
-            indA += 1
-        else:
-            indA = len(numbers)
-    return num_of_cards
+    cards = Dict_create(numbers)                                            #assignment + dict function = loops n times + 1
+    third = ''                                                              #assignment = 1
+    num_of_cards = -1                                                       #assignment = 1
+    indA = 0                                                                #assignment = 1
+    indB = 0                                                                #assignment = 1
+    #loops n times
+    for indA in range(len(numbers)):
+        #loops n times
+        for indB in range(1, len(numbers)):
+            if (numbers[indA] != numbers[indB]):                            #expression = 1
+                third = third_card(numbers[indA], numbers[indB])            #assignment + function = depends on length of card
+                if (third in cards):                                        #expession = 1
+                    if(num_of_cards == -1):                                 #expression = 1
+                        num_of_cards = cards[third] + 1                     #assignment + dictionary access = 2
+                    elif(max(cards[third], indA, indB) < num_of_cards):     #expression + max function = 2
+                        num_of_cards = max(cards[third], indA, indB) + 1    #assignment + max function + addition = 3
+        #print(indB)
+    return num_of_cards                                                     #return statement = 1
 
 
 print(findset(numbers))
